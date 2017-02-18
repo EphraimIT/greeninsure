73ee9a32c1eb6deff129102ed9862af40051139c
developer-ocansey <developer.ocansey@gmail.com>
2017-02-18 12:01:46 +0100

    Add files via upload

---
+++ ephraimit/src/main/java/ephraimit/com/EphraimitApplication.java
@@ -0,0 +1,12 @@
+package ephraimit.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EphraimitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EphraimitApplication.class, args);
+	}
+}
